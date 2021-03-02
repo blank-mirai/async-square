286b963945c83ccc129d517eec40890d0c50c9d7
blank-mirai <joseph.terata@gmail.com>
2021-03-02 17:47:14 -0500

    async square

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-var ball;
+var notBall, database, position, notBallPosition;
 
 function setup(){
+    database = firebase.database();
+
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    notBall = createSprite(250,250,10,10);
+    notBall.shapeColor = "red";
+    notBallPosition = database.ref('ball/position');
+    notBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        WritePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        WritePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        WritePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        WritePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function WritePosition(x,y){
+    database.ref('ball/position').set({
+        'x' : position.x + x, 
+        'y' : position.y + y
+    })
+}
+
+function readPosition(data){
+    position = data.val();
+    notBall.x = position.x;
+    notBall.y = position.y; 
+}
+
+function showError(){
+    console.log("error in writing to the database");
 }
